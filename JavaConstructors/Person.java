86bd79068f3204c132a2a1bf615b9a704df842bb
Aditya12pandey <adityavikrammvm@gmail.com>
2025-06-28 14:37:21 +0530

    Added ; Program to use Copy Cunstructor

---
+++ JavaConstructors/Person.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Person {
+    //Attribute 
+    String name;
+    int age ;
+    Person(String name, int age ){
+        this.name = name;
+        this.age = age;
+    }
+    Person(Person previouPerson){
+        this.name = previouPerson.name;
+        this.age = previouPerson.age;
+    }
+    public void display(){
+        System.out.println("Name = "+name);
+        System.out.print("age = "+age);
+    }
+    public static void main(String[] args){
+        Person person1 = new Person("adtiya" ,21);
+        Person person2 = new Person(person1);
+        person1.display();
+        person2.display();
+    }
+
+
+}
